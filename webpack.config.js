bfc55e5ef0eb10178dcd0347c0183ce3d8f8b10e
bygaga63 <Bygaga63>
2019-11-08 12:13:26 +0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,6 +9,14 @@ module.exports = {
         filename: 'main.js'
     },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin([
+            { from: 'img', to: 'img' },
+            { from: 'css', to: 'css' },
+            // { from: 'index.html', to: '' },
+
+        ]),
     ]
 };
